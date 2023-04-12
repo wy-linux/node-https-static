6719cba18116d93567836b8622771ab5c5478204
wangyu <2514825513@qq.com>
2023-04-12 16:47:27 +0800

    ADD: add resource classification

--- utils.ts
+++ utils.ts
@@ -1,9 +1,17 @@
+const fs = require('fs')
+const path = require('path')
+
+function toPath(item: string) {
+    const stat = fs.statSync(path.resolve(__dirname, 'public', item))
+    return stat.isFile() ? `/${item}` : `/${item}/`
+}
 export function listToHtml(list: Array<string>) {
-    list = list.map((item) =>(
-        `<li style="margin-top: 30px; list-style: none; width: 230px;">
-            <a href=/${item}/>${item}/</a>
-         </li>`
-    ))
+    list = list.map((item) =>{
+       
+        return (`<li style="margin-top: 30px; list-style: none; width: 230px;">
+            <a href=${toPath(item)}>${toPath(item)}</a>
+         </li>`)
+    })
     return (
         `<ul style="display: flex; flex-wrap: wrap; margin-top: 60px">
             ${list.join(' ')}
