150a7d1160d664c5e9705491dd9dfd00b7903e9c
wangyu <2514825513@qq.com>
2023-04-12 17:56:56 +0800

    MOD: change html skeleton and css style

--- utils.ts
+++ utils.ts
@@ -5,16 +5,32 @@ function toPath(item: string) {
     const stat = fs.statSync(path.resolve(__dirname, 'public', item))
     return stat.isFile() ? `/${item}` : `/${item}/`
 }
+
 export function listToHtml(list: Array<string>) {
     list = list.map((item) =>{
-       
-        return (`<li style="margin-top: 30px; list-style: none; width: 230px;">
-            <a href=${toPath(item)}>${toPath(item)}</a>
-         </li>`)
+        return (
+            `<li style="
+                width: 20%;
+                margin: 4% 0 0 4%; 
+                list-style: none; 
+                overflow: hidden;
+                text-overflow: ellipsis;
+                white-space: nowrap;
+                text-align: left;
+            ">
+                <a href=${toPath(item)}>${toPath(item)}</a>
+            </li>`
+        )
     })
-    return (
-        `<ul style="display: flex; flex-wrap: wrap; margin-top: 60px">
-            ${list.join(' ')}
-         </ul>`
-    )
+    let template = (
+        `<ul style="
+            display: flex; 
+            flex-wrap: wrap; 
+            margin-top: 60px;
+            padding: 0;
+        ">
+            ${list.join('')}
+        </ul>`
+    )  
+    return template
 }
